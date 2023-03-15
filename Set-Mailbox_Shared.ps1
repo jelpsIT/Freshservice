1e4423f7689c098c769f3c0abaf5e3c723d1cfb0
jelpsIT <106605874+jelpsIT@users.noreply.github.com>
2023-03-15 12:13:37 +0000

    Update Set-Mailbox_Shared.ps1

--- Set-Mailbox_Shared.ps1
+++ Set-Mailbox_Shared.ps1
@@ -2,7 +2,7 @@ Install-Module ExchangeOnlineManagement
 Import-Module ExchangeOnlineManagement
 
 $Username = "ExOPS_Fresh@leadforensics.onmicrosoft.com"
-$PasswordString = "fuckoffcheckbin"
+$PasswordString = "example string (not providing for obvious reasons)"
 $SecPasswd = ConvertTo-SecureString $PasswordString;
 $Credentials = New-Object System.Management.Automation.PSCredential ($Username, $SecPasswd)
 
