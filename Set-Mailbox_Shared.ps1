e32cf39d5e8a47de6204f02a625ada408dffa5ec
jelpsIT <106605874+jelpsIT@users.noreply.github.com>
2023-07-26 13:49:11 +0100

    Delete Set-Mailbox_Shared.ps1

--- Set-Mailbox_Shared.ps1
+++
@@ -1,9 +0,0 @@
-Install-Module ExchangeOnlineManagement
-Import-Module ExchangeOnlineManagement
-
-$Username = "ExOPS_Fresh@leadforensics.onmicrosoft.com"
-$PasswordString = "example string (not providing for obvious reasons)"
-$SecPasswd = ConvertTo-SecureString $PasswordString;
-$Credentials = New-Object System.Management.Automation.PSCredential ($Username, $SecPasswd)
-
-Connect-ExchangeOnline -Credential $Credentials
